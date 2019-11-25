d8089e111c5e0a4ec24323e69b0f15d126ce9a3f
s5902041620113@email.kmutnb.ac.th <Nattarika-Cha>
2019-11-25 22:14:32 +0700

    frist commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Server_pHProject
-Server_pHProject
---
+++ index.js
@@ -0,0 +1,132 @@
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
+var crypto = require('crypto');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var genRandomString = function (length) {
+    return crypto.randomBytes(Math.ceil(length / 2))
+        .toString('hex')
+        .slice(0, length);
+};
+
+var sha512 = function (password, salt) {
+    var hash = crypto.createHmac('sha512', salt);
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt: salt,
+        passwordHash: value
+    };
+};
+
+function saltHashPassword(userPassword) {
+    var salt = genRandomString(16);
+    var passwordData = sha512(userPassword, salt);
+    return passwordData;
+}
+
+function checkHashPassword(userPassword, salt) {
+    var passwordData = sha512(userPassword, salt);
+    return passwordData;
+}
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017'
+
+MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+    if (err)
+        console.log('Unable to connect to the mongoDB server.Erroe', err);
+    else {
+
+        app.get('/test', (request, response,)=> {
+            var test = {
+                fname: 'Sompot',
+                lname: 'Satongngak'
+            }
+            response.send(test);
+        });
+
+        app.post('/register', (request, response, next) => {
+            var post_data = request.body;
+
+            var plaint_password = post_data.password;
+            var hash_data = saltHashPassword(plaint_password);
+
+            var password = hash_data.passwordHash;
+            var salt = hash_data.salt;
+
+            var fname = post_data.fname;
+            var lname = post_data.lname;
+            var sex = post_data.sex;
+            var username = post_data.username;
+
+            var insertJson = {
+                'username': username,
+                'password': password,
+                'fname': fname,
+                'lname': lname,
+                'sex': sex,
+                'salt': salt
+            };
+            var db = client.db('project')
+
+            db.collection('user')
+                .find({ 'username': username }).count(function (err, number) {
+                    if (number != 0) {
+                        response.json('Email already emists');
+                        console.log('Email already exists');
+                    }
+                    else {
+                        db.collection('user')
+                            .insertOne(insertJson, function (error, res) {
+                                response.json('Registration success');
+                                console.log('Registration success');
+                            })
+                    }
+                })
+        });
+
+        app.post('/login', (request, response, next) => {
+            var post_data = request.body;
+
+            var username = post_data.username;
+            var userPassword = post_data.password;
+
+            var db = client.db('project')
+
+            db.collection('user')
+                .find({ 'username': username }).count(function (err, number) {
+                    if (number == 0) {
+                        response.json('๊Username not exists');
+                        console.log('Email not exists');
+                    }
+                    else {
+                        db.collection('user')
+                            .findOne({ 'username': username }, function (err, user) {
+                                var salt = user.salt;
+                                var hashed_password = checkHashPassword(userPassword, salt).passwordHash;
+                                var encrypted_password = user.password;
+                                if (hashed_password == encrypted_password) {
+                                    response.json('Login success');
+                                    console.log('Login success');
+                                } else {
+                                    response.json('Wrong password');
+                                    console.log('Wrong password');
+                                }
+                            })
+                    }
+                })
+        });
+
+        app.listen(3000, () => {
+            console.log('Connected to MongoDB Server , WebService running on post 3000');
+        })
+    }
+});
+
