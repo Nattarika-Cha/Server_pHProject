bce7a6af35e6f86fef59f95f4069219eeeb5f062
s5902041620113@email.kmutnb.ac.th <Nattarika-Cha>
2019-12-02 01:00:59 +0700

    edit database

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
 module.exports = {
-    DB: 'mongodb+srv://Nattarika:Nattarika0779@cluster0-2pbax.gcp.mongodb.net/phproject?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://Nattarika:Nattarika0779@cluster0-2pbax.gcp.mongodb.net/phproject?authSource=admin&ssl=true&autoReconnect=true'
 }
+
+// module.exports = {
+//     DB: 'mongodb+srv://Nattarika:Nattarika0779@110.168.13.255:27017/phproject?retryWrites=true&w=majority'
+// }
