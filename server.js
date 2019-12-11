68acaabac8a017977aced957e741bf30f686d66c
s5902041620113@email.kmutnb.ac.th <Nattarika-Cha>
2019-12-12 01:59:00 +0700

    test data

--- server.js
+++ server.js
@@ -46,9 +46,10 @@ server.on("error", function (err) {
 server.on("message", function (msg, rinfo) {
     var str = "" + msg;
     var data = str.split(",");
-    var pH = data[0].split("");
+    var pH = data[0].split("").splice(1,".");;
     console.log(pH[0]);
     console.log(pH[1]);
+    console.log(pH[2]);
     var insertSenser = {
         'pH': data[0],
         'moisture': data[1],
