7d1da34090a381c797e0f19b4da5e8747d4d89ff
s5902041620113@email.kmutnb.ac.th <Nattarika-Cha>
2019-12-12 01:26:38 +0700

    save data to database

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     err => { console.log('Can not connect to the database' + err) }
 );
 
+//Routes
 const userRouter = require('./routes/userRoute');
+
+//Model
+const senserModel = require('../model/senserModel');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,34 +36,47 @@ app.get('/', function (req, res) {
 
 // UDP Server
 var dgram = require("dgram");
-var server = dgram.createSocket("udp4"); 
+var server = dgram.createSocket("udp4");
 
 server.on("error", function (err) {
     console.log("server error:\n" + err.stack);
     server.close();
-}); 
+});
 
 server.on("message", function (msg, rinfo) {
     var str = "" + msg;
-    console.log(""+msg);
     var data = str.split(",");
-    console.log(toString(msg).split(",").map(val => val + 1));
-    console.log(toString(msg));
-    console.log(data);
     console.log(data[0]);
     console.log(data[1]);
     console.log(data[2]);
-    console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
-    var ack = new Buffer("Hello ack");
-    server.send(ack, 0, ack.length, rinfo.port, rinfo.address, function (err, bytes) {
-        console.log("sent ACK.");
-    });
-}); 
+    var insertSenser = {
+        'pH': data[0],
+        'moisture': data[1],
+        'latitude': '',
+        'longitude': '',
+        'IMEI': data[2],
+    };
+
+    const senser = new senserModel(insertSenser);
+    senser.save()
+        .then(senser => {
+            var ack = new Buffer("Save success");
+            server.send(ack, 0, ack.length, rinfo.port, rinfo.address, function (err, bytes) {
+                console.log('Save success');
+            });
+        })
+        .catch(err => {
+            var ack = new Buffer("unable to save to database");
+            server.send(ack, 0, ack.length, rinfo.port, rinfo.address, function (err, bytes) {
+                console.log("unable to save to database");
+            });
+        });
+});
 
 server.on("listening", function () {
     var address = server.address();
     console.log("server listening " + address.address + ":" + address.port);
-}); 
+});
 
 server.bind({
     address: '0.0.0.0',
