7dce33140759783c5bce590a1a103b3a5ed3c62c
SompotSE <SompotSE>
2020-01-29 20:48:11 +0700

    date moment

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const moment = require('moment-timezone');
+const dateThailand = moment.tz(Date.now(), "Asia/Bangkok");
+
+console.log(dateThailand); // "2018-08-20T16:35:14.033+07:00"
 
 const userSchema = new Schema({
     username: {
