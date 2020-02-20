737fb9552a79589b02e499fb074513e0c650696c
SompotSE <SompotSE>
2020-02-20 16:22:12 +0700

    test mail

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -196,7 +196,7 @@ async function main() {
 
     let info = await transporter.sendMail({
         from: "Easy Farm Smart Support", // sender address
-        to: "they2539@gmail.com, they1996@hotmail.com, s5802041620203@email.kmutnb.ac.th", // list of receivers
+        to: "they2539@gmail.com, they1996@hotmail.com, s5802041620203@email.kmutnb.ac.th, s5902041620113@email.kmutnb.ac.th", // list of receivers
         subject: "Change Password", // Subject line
         text: "Hello world?....", // plain text body
         html: "<b>Hello world?....</b>" // html body
