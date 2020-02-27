ab48b27d68a21a69af98f0a4eb1185867f756781
SompotSE <SompotSE>
2020-02-27 19:49:24 +0700

    test noti

--- routes/senserRoute.js
+++ routes/senserRoute.js
@@ -5,6 +5,7 @@ var request = require('request');
 
 const senserModel = require('../model/senserModel');
 const deviceModel = require('../model/deviceModel');
+const configModel = require('../model/configModel');
 
 senserRouter.route('/add').post(function (req, res) {
     if (req.body.DevEUI_uplink != undefined) {
@@ -40,25 +41,78 @@ senserRouter.route('/add').post(function (req, res) {
     }
 });
 
-senserRouter.route('/add_test').post(function (req, res) {
-    var insertDataSenser = {
-        'pH': req.body.pH,
-        'moisture': req.body.moisture,
-        'latitude': req.body.latitude,
-        'longitude': req.body.longitude,
-        'pump': req.body.pump,
-        'IMEI': req.body.IMEI
-    };
+// senserRouter.route('/add_test').post(function (req, res) {
+//     (async () => {
+//         // save_senser(req.body.pH, req.body.moisture, req.body.pump, req.body.IMEI)
+//         chack_noti(req.body.pH, req.body.moisture, req.body.latitude, req.body.longitude, req.body.pump, req.body.IMEI).then(() => {
+//             res.json('Save success test');
+//         })
+//     })();
 
-    const data = new senserModel(insertDataSenser);
-    data.save()
-        .then(data => {
-            console.log('Save success test');
-            res.json('Save success test');
-        })
-        .catch(err => {
-            res.status(400).send("unable to save to database");
-        });
+// });
+
+senserRouter.route('/add_test').post(function (req, res) {
+    var pH = req.body.pH;
+    var humidity = req.body.moisture;
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var pump = req.body.pump;
+    var serialDevice = req.body.IMEI;
+    configModel.find({ serialDevice: serialDevice }).countDocuments(function (err, number) {
+        if (number != 0) {
+            configModel.findOne({ serialDevice: serialDevice }, function (err, config) {
+                var pH_low = config.pH_low;
+                var pH_hight = config.pH_hight;
+                var humidity_low = config.humidity_low;
+                var humidity_hight = config.humidity_hight;
+                if (((pH < pH_low) || (pH > pH_hight)) && ((humidity < humidity_low) || (humidity > humidity_hight))) {
+                    console.log("pH and humidity wrong")
+                } else if ((pH < pH_low) || (pH > pH_hight)) {
+                    console.log("pH wrong")
+                } else if ((humidity < humidity_low) || (humidity > humidity_hight)) {
+                    console.log("humidity wrong")
+                } else {
+                    console.log("Normal")
+                }
+            });
+
+            senserModel.findOne({ IMEI: serialDevice }).sort({ '_id': -1 }).exec(function (err, sen_sort) {
+                var insertDataSenser = {
+                    'pH': pH,
+                    'moisture': humidity,
+                    'latitude': latitude,
+                    'longitude': longitude,
+                    'pump': pump,
+                    'IMEI': serialDevice
+                };
+                const data = new senserModel(insertDataSenser);
+                data.save()
+                    .then(data => {
+                        console.log('Save success test');
+                        res.json('Save success test');
+                    })
+                    .catch(err => {
+                        console.log('unable to save to database');
+                    });
+                if (err) throw err;
+                // console.log(pump)
+                // console.log(sen_sort.pump)
+                if (pump != sen_sort.pump) {
+                    if (pump == '0') {
+                        console.log("Close Pump")
+                    } else if (pump == '1') {
+                        console.log("Open Pump")
+                    }
+                } else {
+                    console.log("Normal")
+                }
+
+            })
+        } else {
+            console.log("device not yet config");
+        }
+    });
+
 });
 
 
@@ -124,5 +178,69 @@ senserRouter.route('/pump').post(function (req, res) {
         });
     });
 });
+
+// async function start_noti(pH, humidity, pump, serialDevice) {
+//     await chack_noti(pH, humidity, pump, serialDevice);
+// }
+
+// function save_senser()
+
+// function chack_noti(pH, humidity, latitude, longitude, pump, serialDevice) {
+//     return new Promise(function (resolve, reject) {
+//         configModel.find({ serialDevice: serialDevice }).countDocuments(function (err, number) {
+//             if (number != 0) {
+//                 configModel.findOne({ serialDevice: serialDevice }, function (err, config) {
+//                     var pH_low = config.pH_low;
+//                     var pH_hight = config.pH_hight;
+//                     var humidity_low = config.humidity_low;
+//                     var humidity_hight = config.humidity_hight;
+//                     if (((pH < pH_low) || (pH > pH_hight)) && ((humidity < humidity_low) || (humidity > humidity_hight))) {
+//                         console.log("pH and humidity wrong")
+//                     } else if ((pH < pH_low) || (pH > pH_hight)) {
+//                         console.log("pH wrong")
+//                     } else if ((humidity < humidity_low) || (humidity > humidity_hight)) {
+//                         console.log("humidity wrong")
+//                     } else {
+//                         console.log("Normal")
+//                     }
+//                 });
+
+//                 senserModel.findOne({ IMEI: serialDevice }).sort({ '_id': -1 }).exec(function (err, sen_sort) {
+//                     if (err) throw err;
+//                     console.log(pump)
+//                     console.log(sen_sort.pump)
+//                     if (pump != sen_sort.pump) {
+//                         if (pump == '0') {
+//                             console.log("Close Pump")
+//                         } else if (pump == '1') {
+//                             console.log("Open Pump")
+//                         }
+//                     } else {
+//                         console.log("Normal")
+//                     }
+//                     var insertDataSenser = {
+//                         'pH': pH,
+//                         'moisture': humidity,
+//                         'latitude': latitude,
+//                         'longitude': longitude,
+//                         'pump': pump,
+//                         'IMEI': serialDevice
+//                     };
+//                     const data = new senserModel(insertDataSenser);
+//                     data.save()
+//                         .then(data => {
+//                             console.log('Save success test');
+//                             res.json('Save success test');
+//                         })
+//                         .catch(err => {
+//                             console.log('unable to save to database');
+//                         });
+//                 })
+//             } else {
+//                 console.log("device not yet config");
+//             }
+//         });
+//     })
+// }
 
 module.exports = senserRouter;
