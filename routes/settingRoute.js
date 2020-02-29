cbf7ac28fb842eaf46542227e0121578d5b028bf
SompotSE <SompotSE>
2020-03-01 01:29:30 +0700

    bug setting

--- routes/settingRoute.js
+++ routes/settingRoute.js
@@ -8,8 +8,8 @@ settingRouter.route('/add_setting').post(function (req, res) {
     var token = req.body.token;
     settingModel.find({ 'token': token }).countDocuments(function (err, number) {
         if (number == 0) {
-            console.log("Message : " + req.body.messege);
-            console.log("GPS : " + req.body.gps);
+            // console.log("Message : " + req.body.messege);
+            // console.log("GPS : " + req.body.gps);
             var insertSetting = {
                 // 'sound': req.body.sound,
                 'messege': req.body.messege,
@@ -28,8 +28,8 @@ settingRouter.route('/add_setting').post(function (req, res) {
         } else {
             settingModel.findOne({ 'token': token }, function (err, setting) {
                 // setting.sound = req.body.sound;
-                console.log("Message : " + req.body.messege);
-                console.log("GPS : " + req.body.gps);
+                // console.log("Message : " + req.body.messege);
+                // console.log("GPS : " + req.body.gps);
                 setting.messege = req.body.messege;
                 setting.gps = req.body.gps;
                 setting.save()
